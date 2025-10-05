5b255f6ef76f344e9e2061b7de0dfdec5e830788
Mamethierno Gadiaga <thiernohgradiagram@gmail.com>
2025-10-05 19:24:33 +0000

    Create the GET /albums endpoints

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+} // length = 3, capacity = 3
+
+/*
+Initialize a Gin router with Default
+Use the GET function to associate the GET HTTP method and /albums path with getAlbums function handler
+Use the Run function to attach the router to an http.Server and start the server
+*/
+func main() {
+	var router *gin.Engine = gin.Default() // Initialize a Gin router using Default
+	router.GET("/albums", getAlbums)	   // 	
+	router.Run("localhost:8080")
+}
+
+/*
+This function creates a JSON from the slice of albums and returns the JSON as the response.
+The function takes a variable c or type *gin.Context
+The function uses the gin context to call the IndentedJson  function,
+which serializes the slice into JSON and add it to the response
+*/
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
